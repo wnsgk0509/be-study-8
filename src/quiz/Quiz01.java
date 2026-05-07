5b77a50bede565acf06fbbf6699d587aabfa09a7
wnsgk0509 <rndrndqkd@gmail.com>
2026-05-07 10:25:03 +0900

    퀴즈1 변수 기초

---
+++ src/quiz/Quiz01.java
@@ -0,0 +1,47 @@
+package quiz;
+
+public class Quiz01 {
+	public static void main(String[]args) {
+		//1번
+		int month;
+		
+		//2번
+		int day = 1;
+		
+		//3번
+		month = 5;
+		day = 7;
+		System.out.println("오늘은 "+month+"월 "+day+"일"+" 입니다.");
+		
+		
+		//4번
+		String name;
+		name = "이준하";
+		System.out.println(name);
+		
+		//5번
+		String tel = "010-4611-1339";
+		System.out.println("전화번호는 "+tel+" 입니다.");
+		
+		/*
+		 * ##	6번	##
+		 * 답: a, d, e
+		 * 이유: b, f= 변수명에 특수 기호는 #,_ 만 사용가능하다
+		 * 		c	= 변수명은 숫자로 시작할 수 없다.
+		 * 		g 	= 변수명으로 예약어는 사용이 불가능 하다 
+		 * 
+		 * 		d= 두 개 이상의 단어가 결합될때는 관례상 newMember 라고 작성한다
+		 * 		a, e= $를 변수명으로 사용가능은 하나 개발자가 직접 사용하지는 않는다
+		 * 			(컴파일러가 자동 생성하는 변수에 주로 사용됨.)
+		 * 
+		 * 
+		 * ##	7번	##
+		 * 답: c
+		 * 
+		 * 
+		 * ##	8번	##
+		 * 답: b
+		 * 
+		 * */
+	}
+}
