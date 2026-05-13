264f598e37d7c227475e0041f72083b94727e530
wnsgk0509 <rndrndqkd@gmail.com>
2026-05-13 16:01:17 +0900

    참조타입변수 기본개념

---
+++ src/be_study/refer/Refer01.java
@@ -0,0 +1,59 @@
+package be_study.refer;
+
+import java.util.Scanner;
+
+public class Refer01 {
+
+	public static void main(String[] args) {
+
+		
+		//기존타입 참조타입
+		
+		int n;			//값X 기존타입변수
+		int num = 0;	//값O 기존타입변수
+		
+		String s1;		//값X 참조타입변수
+		String s2 = null;// 참조하는게 없다! null
+				
+//		System.out.println(n);
+		System.out.println(s2);
+		
+		Scanner scanner = new Scanner(System.in);
+		String t1 = "사과";
+		String t2 = "사과";
+		String t3 = scanner.nextLine();
+		String t4 = new String("사과");
+		
+		System.out.println(t1 == t2);
+		System.out.println(t1.equals(t2));
+		
+		System.out.println(t1 == t3);
+		System.out.println(t1.equals(t3));
+		
+		System.out.println(t1 == t4);
+		System.out.println(t1.equals(t4));
+		
+		
+		System.out.println("---------------------");
+		
+		String ne = null;
+		System.out.println( ne == null);
+		
+		//boolean b = true false			b == true
+		
+//		System.out.println( ne.equals("사과"));
+		// NullPointerException
+		
+		// 값 복사
+		int n1 = 10;
+		int n2 = n1;
+		System.out.println(n1);
+		System.out.println(n2);
+		
+		String str1 = "커피";
+		String str2 = str1;
+		System.out.println(str1);
+		System.out.println(str2);
+	}
+
+}
