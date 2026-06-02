9e08db450da640644d7d160f5e7217feb0303743
wnsgk0509 <rndrndqkd@gmail.com>
2026-06-02 10:27:16 +0900

    퀴즈38 클래스 상속(+추상화 개념)

---
+++ src/be_study/quiz/Quiz38/v1/Unit.java
@@ -0,0 +1,91 @@
+package be_study.quiz.Quiz38.v1;
+
+public class Unit {
+	int x,y;
+	void move(int x, int y) {
+
+	}
+	void stop() {
+		System.out.println("현재위치 ("+x + "," + y + ") 에서 멈춘다");
+	}
+
+
+	public static void main(String[]args) {
+		
+		Marine u1 = new Marine();
+		Tank u2 = new Tank();
+		Dropship u3 = new Dropship();
+		
+		Unit[] arr  = {u1, u2, u3};
+		
+		for(Unit u : arr) {
+			u.move(10, 20);
+			u.stop();
+		}
+		
+		u1.stimPack();
+		u2.changeMode();
+		u3.load();
+		u3.unload();
+		
+		
+		
+	}
+	
+	static class Marine extends Unit{ // 보병
+//		int x, y; // 현재 위치
+		void move(int x, int y) {
+			super.x += x;
+			super.y += y;
+			System.out.println("x:"+x+" y:"+y+" 만큼 걷는다");
+		} //지정된 위치로 이동
+		
+//		void stop() {
+//				/* */
+//		} //현재 위치에 정지
+		void stimPack() {
+			System.out.println("스팀팩을 사용한다");
+		} //스팀팩을 사용한다
+	}
+	
+	static class Tank extends Unit{ // 탱크
+		
+//		int x, y; // 현재 위치
+		void move(int x, int y) { 
+			super.x += x;
+			super.y += y;
+			System.out.println("x:"+x+" y:"+y+" 만큼 바퀴를 구른다");
+		} //지정된 위치로 이동
+		
+//		void stop() {
+//			/* */ 
+//		} //현재 위치에 정지
+		
+		void changeMode() { 
+			System.out.println("공격모드를 변환한다");
+		} //공격모드를 변환한다
+	}
+	
+	static class Dropship extends Unit{ // 수송선
+		
+//		int x, y; // 현재 위치
+		void move(int x, int y) {
+			super.x += x;
+			super.y += y;
+			System.out.println("x:"+x+" y:"+y+" 만큼 날아간다");
+		} //지정된 위치로 이동
+		
+//		void stop() { 
+//			/* */ 
+//			} //현재 위치에 정지
+		
+		void load() { /* .*/
+			
+		} //선택된 대상을 태운다
+		
+		void unload() {
+			/* .*/ 
+		} //선택된 대상을 내린다
+	}
+	
+}
